02880def4a159798db3921ecb7cddcacca9b3931
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-21 15:49:12 -0600

    Create an input form for a search term

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState,useEffect } from 'react';
-import ResultsTable from './componets/ResultsTable/ResultsTable';
-
+import React, { useState,useEffect } from 'react'
+import ResultsTable from './componets/ResultsTable/ResultsTable'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 export const App = () => {
   const [restaurants, setRestaurants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch(`https://code-challenge.spectrumtoolbox.com/api/restaurants`, { headers: {
@@ -15,9 +16,27 @@ export const App = () => {
     .catch(err => console.log(err));
   }, [])
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchTerm)
+
+    // filter restaurants based on serch term
+  }
+
   return (
     <div className="App">
       <h1>Food Finder</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='search'>Search:</label>
+        <input 
+          id='search'
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder='Enter restaurant name, city or genre'
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
       <ResultsTable restaurants={restaurants} />
     </div>
   );
