01bb8ec5e23b9878c2590c544ee636b1f4f3e9e4
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-22 10:21:34 -0600

    Create a sorted array of states to pick from

---
+++ src/utils/stateOptions.js
@@ -0,0 +1,13 @@
+export const stateOptions = (restaurantArr) => {
+    let statesObj = {}; // using an object for const time lookup
+
+    for (let i = 0; i < restaurantArr.length; i++) {
+        if (!statesObj[restaurantArr[i].state]) {
+            statesObj[restaurantArr[i].state] = restaurantArr[i].state;
+        }
+    }
+    let sortedStates = Object.keys(statesObj).sort();
+    return sortedStates;
+}
+
+export default stateOptions;
