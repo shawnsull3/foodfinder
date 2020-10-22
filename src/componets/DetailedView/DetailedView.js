4439a9c3872f9504cdbcaf83bb1047f928de2eb2
Shawn Sullivan <sullivansm3@gmail.com>
2020-10-22 13:55:41 -0600

    Show resturant details when clicked

---
+++ src/componets/DetailedView/DetailedView.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './style.css'
+
+export const DetailedView = ({ restaurant, showDetails }) => {
+    console.log(restaurant);
+
+    return (
+        <div>
+            <div className='modal-cover'>
+            </div>
+            <div className='modal-card'>
+                <button className='close-btn' onClick={() => showDetails(false)}>
+                    x
+                </button>
+                
+                <div className='restaurant-details'>
+                    <h2>{restaurant.name}</h2>
+                    <p>Address: {restaurant.address1}</p>
+                    <p>{restaurant.city}, {restaurant.state} {restaurant.zip}</p>
+                    <p>Hours: {restaurant.hours}</p>
+                    <p>Phone Number: {restaurant.telephone}</p>
+                    <p>Attire: {restaurant.attire}</p>
+                    <p>Website: 
+                        <a className='website' href={restaurant.website} target='_blank' rel='noopener noreferrer'>
+                            {restaurant.website}
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DetailedView;
--- src/componets/ResultsTable/ResultsTable.js
+++ src/componets/ResultsTable/ResultsTable.js
@@ -1,34 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DetailedView from '../DetailedView/DetailedView'
 import Table from 'react-bootstrap/Table'
 import './style.css'
 
 export const ResultsTable = ({ restaurants }) => {
+    const [details, showDetails] = useState(false);
+    const [restaurantIndex, setRestaurantIndex] = useState(0);
+
     return (
-        <Table striped bordered hover variant='dark'>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Phone Number</th>
-                    <th>Genre</th>
-                </tr>
-            </thead>
-            <tbody>
-                {restaurants.map((restaurant, i) => {
-                    const spacedGenres = restaurant.genre.split(',').join(', ');
-                    return (
-                        <tr key={restaurant.id}>
-                            <td>{restaurant.name}</td>
-                            <td>{restaurant.city}</td>
-                            <td>{restaurant.state}</td>
-                            <td>{restaurant.telephone}</td>
-                            <td>{spacedGenres}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </Table >
+        <div>
+            {details && 
+                <DetailedView 
+                  restaurant={restaurants[restaurantIndex]}
+                  showDetails={showDetails}
+                />
+            }
+
+            <Table striped bordered hover variant='dark'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Phone Number</th>
+                        <th>Genre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {restaurants.map((restaurant, i) => {
+                        const spacedGenres = restaurant.genre.split(',').join(', ');
+                        return (
+                            <tr 
+                              key={restaurant.id} 
+                              onClick={(e) => {
+                                  console.log(i)
+                                  setRestaurantIndex(i);
+                                  showDetails(true);
+                              }}
+                            >
+                                <td>{restaurant.name}</td>
+                                <td>{restaurant.city}</td>
+                                <td>{restaurant.state}</td>
+                                <td>{restaurant.telephone}</td>
+                                <td>{spacedGenres}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table >
+        </div>
     )
 }
 
